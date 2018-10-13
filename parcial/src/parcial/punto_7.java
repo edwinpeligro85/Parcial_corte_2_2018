7b4d62bdc222dedc4f8e130f6a57a5bb96e94f00
edwinpeligro85 <43562729+edwinpeligro85@users.noreply.github.com>
2018-10-13 14:24:23 -0500

    punto 7 del parcial
    Edwin Serna
    Emmanuel serna

--- parcial/src/parcial/punto_7.java
+++ parcial/src/parcial/punto_7.java
@@ -1,79 +1,69 @@
 package parcial;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class punto_7 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Inicio del programa.
+        int matA[][] = new int [3][3];                                          //Creo la matriz A.
+        int matB[][] = new int [3][3];                                          //Creo la matriz B.
+        int matR[][] = new int [3][3];                                          //Creo una matriz para guardar el resultado.
+        int fila, columna;                                                      //Declaro dos variables tipo entero.
         
-        int matA[][] = new int [3][3];
-        int matB[][] = new int [3][3];
-        int matR[][] = new int [3][3];
-        int fila, columna;
-        
-        matA[0][0] = 1;
-        matA[0][1] = 4;
-        matA[0][2] = 7;
-        matA[1][0] = 2;
-        matA[1][1] = 5;
-        matA[1][2] = 8;
-        matA[2][0] = 3;
-        matA[2][1] = 6;
-        matA[2][2] = 9;
-        for (int i = 0; i < matA.length; i++) {
-            int[] is = matA[i];
-            System.out.println(Arrays.toString(is));
-        }
-        System.out.println("\n");
-        matB[0][0] = 1;
-        matB[0][1] = -1;
-        matB[0][2] = 2;
-        matB[1][0] = 2;
-        matB[1][1] = -1;
-        matB[1][2] = 2;
-        matB[2][0] = 3;
-        matB[2][1] = -3;
-        matB[2][2] = 0;
-        for (int i = 0; i < matB.length; i++) {
-            int[] is = matB[i];
-            System.out.println(Arrays.toString(is));
-        }
-        System.out.println("\n");
-        
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);                                   //Creo un objeto tipo Scanner para tomar datos por teclado.
          
-        System.out.println("Ingrese los datos de la Matriz A :");
-         
-        for (fila=0; fila < matA.length; fila++){
-            for (columna = 0; columna < matA[fila].length; columna++){
-                System.out.print("Escribir valor " + fila + " , " + columna + " : ");
-                matA[fila][columna]= sc.nextInt();
+        System.out.println("Ingrese los datos de la Matriz A");               //Muestro por pantalla un mensaje indicando la matriz.
+        for (fila=0; fila < matA.length; fila++){                               //Recorro las filas dela matriz (matA).
+            for (columna = 0; columna < matA[fila].length; columna++){          //Recorro las columnas de la matriz (matA).
+                System.out.print("Escribir valor "+fila+" , "+columna+" : ");   //Muestro por pantalla un mensaje indicando la posición.
+                matA[fila][columna]= sc.nextInt();                              //Tomo por teclado los datos y los almaceno en la matriz(matA).
             }
-        }           
-        System.out.println("Ingrese los datos de la Matriz B :");  
-        for (fila = 0; fila < matB.length; fila++){
-            for (columna=0; columna < matB[fila].length; columna++){
-                System.out.print("Escribir valor " + fila + " , " + columna + " : ");
-                matB [fila][columna]= sc.nextInt();
+        }System.out.println("\n");                                              //Salto de linea entre matriz.
+        
+        System.out.println("Ingrese los datos de la Matriz B");               //Muestro por pantalla un mensaje indicando la matriz.
+        for (fila = 0; fila < matB.length; fila++){                             //Recorro las filas dela matriz (matB).
+            for (columna=0; columna < matB[fila].length; columna++){            //Recorro las columnas de la matriz (matB).
+                System.out.print("Escribir valor "+fila+" , "+columna+" : ");   //Muestro por pantalla un mensaje indicando la posición.
+                matB [fila][columna]= sc.nextInt();                             //Tomo por teclado los datos y los almaceno en la matriz(matB).
             }
-        }
+        }System.out.println("\n");                                              //Salto de linea entre matriz.
+        
+        //Cilo para hacer la multiplicación
         for (fila = 0; fila < matR.length; fila++){
             for (columna = 0; columna < matR[fila].length; columna++){
                 for (int vueltas = 0; vueltas < matR.length; vueltas++) {
                     matR[fila][columna] += matA[fila][vueltas] * matB[vueltas][columna]; 
                 }
             }
-        }
-            
-        System.out.println("Matriz resultante de la multiplicación :");
-        for (fila=0;fila<=2;fila++){
-            for (columna=0;columna<=2;columna++) {
-                System.out.print(matR[fila][columna] + " ");
+            System.out.println("");
+        }System.out.println("");
+        
+        //Ciclo que Muestra la matriz A.
+        System.out.println("Matriz A:");
+        for (fila = 0; fila < matA.length; fila++){
+            for (columna = 0;columna < matA[fila].length; columna++) {
+                System.out.print("\t"+matA[fila][columna]);
             }
             System.out.println("");
-            
+        }System.out.println("\t\t");
+        
+        //Ciclo que muestra la matriz B.
+        System.out.println("Matriz B:");
+        for (fila = 0; fila < matB.length; fila++){
+            for (columna = 0;columna < matB[fila].length; columna++) {
+                System.out.print("\t"+matB[fila][columna]);
+            }
+            System.out.println("");
+        }System.out.println("\t\t");
+        
+        //Ciclo que muestra el resultado.
+        System.out.println("Resultado de la multiplicación :");
+        for (fila = 0; fila < matR.length; fila++){
+            for (columna = 0;columna < matR[fila].length; columna++) {
+                System.out.print("\t"+matR[fila][columna]);
+            }
+            System.out.println("");
         }
     }
-}
+}//Fin del prigrama
